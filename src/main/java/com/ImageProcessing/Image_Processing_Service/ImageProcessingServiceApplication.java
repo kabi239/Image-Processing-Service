64f8843640e4ed1a689e28cee56a45298422c545
kabi239 <kavyasharma239@gmail.com>
2025-02-04 19:34:30 +0530

    Initial commit

---
+++ src/main/java/com/ImageProcessing/Image_Processing_Service/ImageProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ImageProcessing.Image_Processing_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageProcessingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ImageProcessing/Image_Processing_Service/ImageProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ImageProcessing.Image_Processing_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
